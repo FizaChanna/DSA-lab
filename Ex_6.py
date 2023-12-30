adf956a17135bd2a5377ac5dbbea71c9a58314e1
FizaChanna <khanamch268@gmail.com>
2023-12-30 23:48:20 +0500

    Add new file

---
+++ Ex_6.py
@@ -0,0 +1,4 @@
+Exercise 6: Advanced List Operations
+1.Implement a function to find the intersection of two lists.
+2.Write a program to find the union of two lists.
+3.Create a program to implement a simple stack using a list.
